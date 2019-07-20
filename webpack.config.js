310e6138c19d43d677cddc97d1e77f2f9e8b985d
Myko <m.miparanum@gmail.com>
2019-07-20 18:57:07 +0800

    Setup production web pack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,52 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return {
+        entry:'./src/app.js',
+        // Here the application starts executing
+        // and webpack starts bundling
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader', //allows to run babel under certain condition
+                test: /\.js$/, //whenever you see a .js file that is not in the node_modules folder, go run it through babel
+                exclude: /node_modules/
+            },{
+                test:/\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                    sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map': 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+};
 
 console.log(__dirname);
-module.exports = {
-    entry:'./src/app.js',
-    // Here the application starts executing
-    // and webpack starts bundling
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader', //allows to run babel under certain condition
-            test: /\.js$/, //whenever you see a .js file that is not in the node_modules folder, go run it through babel
-            exclude: /node_modules/
-        },{
-            test:/\.s?css$/,
-            use: [
-                 'style-loader',
-                 'css-loader',
-                 'sass-loader'
-            ]
-        }]
-        
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
